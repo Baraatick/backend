916fff77137107737112bf4870e77589bf2fea85
Baraat <kahimalaevbaraat@gmail.com>
2024-04-22 12:43:59 +0500

    first

---
+++ src/api/extraproduct/services/extraproduct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * extraproduct service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::extraproduct.extraproduct');
